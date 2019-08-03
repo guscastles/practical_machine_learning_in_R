27789f13366260fb6309c6bd22a30415d1490899
guscastles <guscastles@gmail.com>
2019-08-03 11:34:54 +1000

    Added project files

--- spam.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-from __future__ import print_function
-
-import pandas as pd
-spam = pd.read_csv('spam.csv')
-prediction = spam['your'].map(lambda x: "spam" if x > 0.5 else "nonspam")
-pred_table = pd.crosstab(prediction, spam['type'], rownames=['prediction'], colnames=["type"])
-accuracy = pred_table/len(spam['type'])
-print(round(accuracy.loc['nonspam', 'nonspam'] + accuracy.loc['spam', 'spam'], 3))
